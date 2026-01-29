2c775290e548dff71258c97e5d90923f6b64beb4
tomoki013 <tomoki_ttttt@icloud.com>
2026-01-29 12:47:52 +0900

    謎の変更をコミット

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,11 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "images.unsplash.com",
       },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "/**",
+      },
     ],
   },
 };
